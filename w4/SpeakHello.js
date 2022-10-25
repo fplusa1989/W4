b07b9ae0fa9766f1ebae2029c95240c4300d87dc
fplusa1989 <115445680+fplusa1989@users.noreply.github.com>
2022-10-25 13:54:52 -0600

    Delete w4 directory

--- w4/SpeakHello.js
+++
@@ -1,7 +0,0 @@
-(function(window){
-	var speakWord = "Hello";
-	var helloSpeaker = function speak(name) {
-		console.log(speakWord + " " + name)
-	};
-	window.helloSpeaker = helloSpeaker;
-}) (window);
